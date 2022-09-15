6e1552a6372df4e634801ff0ef6b03df1ab34ae2
Your Name <you@example.com>
2022-09-15 14:26:19 -0500

    Update

--- server.js
+++ server.js
@@ -7,8 +7,25 @@ const app = express();
 //connect database
 connectDatabase();
 
+//configure middleware
+app.use(express.json({extended: false}));
+
 //API endpoints
+/**
+ * @route GET/
+ * @desc Test endpoint
+ */
 app.get('/', (req, res) => res.send('http get request sent to root api endpoint'));
+
+/**
+ * @route GET /
+ * @desc Test endpoint
+ */
+
+app.post('/api/users', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
 
 //connection listener
 app.listen(3000, () => console.log('Express server running on port 3000'));
