1a8750bd2bd547ca247461e403f607a26142e272
unknown <onealr4@gmatc.matc.edu>
2022-10-06 12:10:47 -0500

    stage

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import connectDatabase from './config/db';
 import { check, validationResult } from 'express-validator';
 import cors from 'cors';
+import bcrypt from 'bcryptjs';
+import User from '.models/User';
 
 //initialize express application
 const app = express();
@@ -35,13 +37,39 @@ app.post('/api/users',
     check('email', 'Please enter a valid email').isEmail(),
     check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
 ],
-(req, res) => {
+async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(422).json({ errors: errors.array() });
     } else {
-    console.log(req.body);
-    res.send(req.body);
+    const { name, email, password } = req.body;
+    try {
+        //check if user exists
+        let user = await User.findOne({ email: email});
+        if (user) {
+            return res  
+                .status(400)
+                .json({errors: [{ msg: 'User already exists' }] });
+        }
+
+        //create a new user
+        user = new User({
+            name: name,
+            email: email,
+            password: password
+        });
+
+        //encrypt the password
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+
+        //save to the db and return
+        await user.save();
+        res.send('User successfully registered');
+    } catch (error) {
+        res.status(500).send('Server error');
+    }
+    
     }
     
 });
