d2056d05223961d0856d1721050e96d6d9b5ac8a
Rose ONeal <onealr4@gmatc.matc.edu>
2022-11-01 14:31:40 -0500

    Activity-7 Fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
-import axios from 'axios';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Register from './components/Register/Register';
 import Login from './components/Login/Login';
+import axios from 'axios';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 class App extends React.Component {
+
   state = {
-    data: null
+    data: null,
+    token: null,
+    user: null
   }
 
   componentDidMount() {
@@ -20,40 +23,96 @@ class App extends React.Component {
       .catch((error) => {
         console.error(`Error fetching data: ${error}`);
       })
+
+    this.authenticateUser();
+  }
+
+  authenticateUser = () => {
+    const token = localStorage.getItem('token');
+
+    if (!token) {
+      localStorage.removeItem('user');
+      this.setState({ user: null });
+    }
+
+    if (token) {
+      const config = {
+        headers: {
+          'x-auth-token': token
+        }
+      }
+      axios.get('http://localhost:5000/api/auth', config)
+        .then((response) => {
+          localStorage.setItem('user', response.data.name)
+          this.setState({ user: response.data.name })
+        })
+        .catch((error) => {
+          localStorage.removeItem('user');
+          this.setState({ user: null });
+          console.error(`Error logging in: ${error}`);
+        })
+    }
+  }
+
+  logOut = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    this.setState({ user: null, token: null });
   }
 
   render() {
+    let { user, data } = this.state;
+    const authProps = {
+      authenticateUser: this.authenticateUser
+    }
     return (
       <Router>
         <div className="App">
           <header className="App-header">
-            <h1>GoodThings</h1>
+            <h1>Good Things</h1>
             <ul>
               <li>
                 <Link to="/">Home</Link>
               </li>
               <li>
-                <Link to="/register">Register</Link>
+                <Link to="/Register">Register</Link>
               </li>
               <li>
-                <Link to="/login">Login</Link>
+                {user ?
+                  <Link to="" onClick={this.logOut}>Log out</Link> :
+                  <Link to="/Login">Log in</Link>
+                }
               </li>
             </ul>
           </header>
           <main>
             <Route exact path="/">
-              {this.state.data}
+              {user ?
+                <React.Fragment>
+                  <div>Hello {user}!</div>
+                  <div>{data}</div>
+                </React.Fragment> :
+                <React.Fragment>
+                  Please Register or Login
+                </React.Fragment>
+              }
+
             </Route>
             <Switch>
-              <Route path="/register" component={Register} />
-              <Route path="/login" component={Login} />
+              <Route
+                exact path="/Register"
+                render={() => <Register {...authProps} />} />
+              <Route
+                exact path="/Login"
+                render={() => <Login {...authProps} />} />
             </Switch>
           </main>
+
         </div>
       </Router>
     );
   }
-}
 
+}
 
 export default App;
