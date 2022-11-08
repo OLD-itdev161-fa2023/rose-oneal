97f8dda2850ea102b474db43199eb6c416326aa9
Rose ONeal <onealr4@gmatc.matc.edu>
2022-11-08 17:46:10 -0600

    activity-9

---
+++ rose-oneal/server.js
@@ -0,0 +1,215 @@
+import express from 'express';
+import connectDatabase from './config/db';
+import { check, validationResult } from 'express-validator';
+import cors from 'cors';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import config from 'config';
+import User from './models/User';
+import Post from './models/Post';
+import auth from './middleware/auth';
+
+//initialize express application
+const app = express();
+
+//connect database
+connectDatabase();
+
+//configure middleware
+app.use(express.json({extended: false}));
+app.use(
+    cors({
+        origin: 'http://localhost:5000'
+    })
+);
+
+//API endpoints
+/**
+ * @route GET/
+ * @desc Test endpoint
+ */
+app.get('/', (req, res) => res.send('http get request sent to root api endpoint'));
+
+/**
+ * @route GET /
+ * @desc Test endpoint
+ */
+
+app.post('/api/users', 
+[
+    check('name', 'Please enter your name').not().isEmpty(),
+    check('email', 'Please enter a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+],
+async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    } else {
+    const { name, email, password } = req.body;
+    try {
+        //check if user exists
+        let user = await User.findOne({ email: email});
+        if (user) {
+            return res  
+                .status(400)
+                .json({errors: [{ msg: 'User already exists' }] });
+        }
+
+        //create a new user
+        user = new User({
+            name: name,
+            email: email,
+            password: password
+        });
+
+        //encrypt the password
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+
+        //save to the db and return
+        await user.save();
+        
+        //generate and return jwt token
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+
+        jwt.sign(
+            payload,
+            config.get('jwtSecret'),
+            { expiresIn: '10hr' },
+            (err, token) => {
+                if (err) throw err;
+                res.json({ token: token });
+            }
+        );
+    
+    } catch (error) {
+        res.status(500).send('Server error');
+    }
+    
+    }
+    
+});
+
+//Post endpoints
+
+/**
+ * @route Post api/posts
+ * @deasc Create post
+ */
+
+app.post(
+    '/api/posts',
+    [
+        auth,
+        [
+            check('title', 'Title text is required')
+                .not()
+                .isEmpty(),
+            check('body', 'Body text is required')
+                .not()
+                .isEmpty()
+        ]
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()){
+            res.status(400).json({ errors: errors.array() });
+        } else {
+            const { title, body } = req.body;
+            try {
+                //get the user who created the post
+                const user = await User.findById(req.user.id);
+
+                //create a new post
+                const post = new Post({
+                    user: user.id,
+                    title: title,
+                    body: body
+                });
+
+                //save the db and return
+                await post.save();
+
+                res.json(post);
+            } catch (error) {
+                console.error(error);
+                res.status(500).send('Server error');
+            }
+        }
+    }
+);
+
+/**
+ * @route GET api/posts
+ * @desc Get posts
+ */
+
+app.get('/api/posts', auth, async (req, res) => {
+    try {
+        const posts = await Post.find().sort({ date: -1 });
+
+        res.json(posts);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server error');
+    }
+});
+
+/**
+ * @route GET api/endpoints/:id
+ * @desc GET post
+ */
+
+app.get('/api/posts/:id', auth, async (req, res) =>  {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        //make sure the post was found
+        if (!post) {
+            return res.status(404).json({ msg: 'Post not found' })
+;        }
+
+        res.json(post);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server error');
+    }
+});
+
+/**
+ * @route DELETE api/posts/:id
+ * @desc Delete a post
+ */
+
+app.delete('/api/posts/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        //make sure the post was found
+        if (!post){
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+
+        //make sure the request user created the post
+        if (post.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'User not authorized' });
+        }
+
+        await post.remove();
+
+        res.json({msg: 'Post removed'});
+    } catch (error) {
+        console.error(error);
+            res.status(500).send('Server error');
+        }
+    
+});
+
+//connection listener
+const port = 5000;
+app.listen(port, () => console.log(`Express server running on port ${port}`));
